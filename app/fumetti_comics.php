eb6f6df0e370698f7ed437bce58ca00ce46392ee
gioiascari <gioia.ascari96@gmail.com>
2022-05-25 16:08:52 +0200

    add file migration

---
+++ app/fumetti_comics.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class fumetti_comics extends Model
+{
+    //
+}
---
+++ database/migrations/2022_05_25_140231_create_fumetti_comics_table.php
@@ -0,0 +1,39 @@
+<?php
+    // php artisan make:model 'nome_model' -m
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFumettiComicsTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fumetti_comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('price');
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fumetti_comics');
+    }
+}
